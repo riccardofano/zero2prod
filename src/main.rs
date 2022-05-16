9e09af2597cdcaab9260284379741c2864f89e44
riccardofano <rcr.fano@gmail.com>
2022-05-16 19:58:01 +0200

    refactor test organization and startup logic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
-
 use zero2prod::{
     configuration::get_configuration,
-    email_client::EmailClient,
-    startup::run,
+    startup::Application,
     telemetry::{get_subscriber, init_subscriber},
 };
 
@@ -14,27 +10,8 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("failed to read configuration");
-    let connection = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
 
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("invalid sender email address");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address).expect("failed to bind to random port");
-
-    run(listener, connection, email_client)?.await
+    Ok(())
 }
