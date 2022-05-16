16d8609550f2a5ad1e0a5a0d6bd27a27700fa109
riccardofano <rcr.fano@gmail.com>
2022-05-16 11:02:53 +0200

    add tests for subscriber name

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -131,7 +131,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_empty() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
 
@@ -151,9 +151,9 @@ async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
             .expect("failed to execute request");
 
         assert_eq!(
-            200,
+            400,
             response.status().as_u16(),
-            "the API did not return a 200 OK when the payload was {description}"
+            "the API did not return a 400 Bad Request when the payload was {description}"
         );
     }
 }
